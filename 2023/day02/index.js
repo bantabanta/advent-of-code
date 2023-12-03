9cb0b06eff6d3adcddf5c4de4eddb421ab564ecf
Tim Banta <timbanta@me.com>
2023-12-02 17:05:37 -0800

    part one complete

--- 2023/day02/index.js
+++ 2023/day02/index.js
@@ -1,12 +1,32 @@
 const fs = require("fs");
-const { log } = require("console");
 
 let data = fs.readFileSync("./input.txt", "utf8").trim().split("\n");
 let test = fs.readFileSync("./test.txt", "utf8").trim().split("\n");
 
-function partOne(input) {}
+function partOne(input) {
+  const cubeMap = { red: 12, green: 13, blue: 14 };
 
-function partTwo(input) {}
+  let possibleGames = input.map((line) => {
+    let game = Number(line.match(/\d+/)[0]);
 
-// log("Part One:", partOne(data));
-// log("Part Two:", partTwo(data));
+    cubePulls = line.split(":")[1].replace(/;/g, ",").split(",");
+
+    let validCount = cubePulls.every((pull) => {
+      const count = pull.match(/\d+/);
+      const color = pull.split(count)[1].trim();
+      return cubeMap[color] >= count;
+    });
+
+    return validCount ? game : 0;
+  });
+  return possibleGames.reduce((a, b) => a + b);
+}
+
+console.log("Part One:", partOne(data));
+
+function partTwo(input) {
+  let cubePower = input.map((line) => {
+    console.log("part two");
+  });
+}
+console.log("Part Two:", partTwo(test));
